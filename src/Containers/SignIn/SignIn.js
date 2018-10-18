398d7cb8a4fc5b46d1f29275e42ba673c6335b62
Apollo108 <oxys87@gmail.com>
2018-10-18 17:33:15 +0300

    - Added sign in modal

--- src/Containers/SignIn/SignIn.js
+++ src/Containers/SignIn/SignIn.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles'
-import { Button, Icon } from '@material-ui/core/';
+import { Button, Modal, Typography, TextField } from '@material-ui/core/';
 import styles from './sign_in_styles';
 
 export default withStyles(styles)(
     class SignIn extends Component {
+      state = {
+        isModalOpen: false,
+      }
+      handleOpen = () => {
+        this.setState({ isModalOpen: true });
+      };
+
+      handleClose = () => {
+        this.setState({ isModalOpen: false });
+      };
       render() {
         const { classes } = this.props;
         return (
           <div className={classes.signInWrapper}>
-            <Button size="medium">
+            <Button size="medium" onClick={this.handleOpen}>
               Sign in
             </Button>
-            <Button size="medium">
+            <Button size="medium" onClick={this.handleOpen}>
               Sign up
             </Button>
+            <Modal
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+              open={this.state.isModalOpen}
+              onClose={this.handleClose}
+              className={classes.backDrop}
+            >
+              <div className={classes.modal}>
+                <Typography variant="h6" id="modal-title">
+                  Sign in
+                </Typography>
+                <TextField
+                  id="standard-email-input"
+                  label="Email"
+                  type="email"
+                  autoComplete="current-email"
+                  margin="normal"
+                />
+                <TextField
+                  id="standard-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  margin="normal"
+                  className={classes.pwdInput}
+                />
+                <Button size="medium" className={classes.submitBtn}>
+                  Submit
+                </Button>
+                <Typography className={classes.enterPwdText}>
+                  Please, enter your email and password
+                </Typography>
+                <Button size="medium" className={classes.haveNoAccBtn}>
+                  Don't have an account?
+                </Button>
+              </div>
+            </Modal>
           </div>
         );
       }
--- src/Containers/SignIn/sign_in_styles.js
+++ src/Containers/SignIn/sign_in_styles.js
@@ -1,2 +1,33 @@
 export default {
+  backDrop: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+    width: 250,
+    height: 300,
+    borderRadius: 2,
+    backgroundColor: 'white',
+    position: 'absolute',
+    outline: 'none',
+  },
+  pwdInput: {
+    marginTop: 0,
+  },
+  submitBtn: {
+    backgroundColor: '#eee',
+    margin: '10px 0',
+  },
+  enterPwdText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+  haveNoAccBtn: {
+    color: 'gray',
+  }
 }
